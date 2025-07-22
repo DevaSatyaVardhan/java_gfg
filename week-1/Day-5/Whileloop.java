90d8d661f41939d2db08e157213ce4e6281c6ded
DevaSatyaVardhan <devachinta051@gmail.com>
2025-07-23 00:16:57 +0530

    Added week-2 folder to the repo

---
+++ week-1/Day-5/Whileloop.java
@@ -0,0 +1,13 @@
+package Day-5;
+
+public class Whileloop {
+    public static void main(String[] args) {
+        // Print "GeeksforGeeks" n times using a while loop
+        int n = 5; // You can change this value to print more or fewer times
+        int i = 0;
+        while (i < n) {
+            System.out.println("GeeksforGeeks");
+            i++;
+        }
+    }
+}
---
+++ week-1/Day-6/Inputinteger.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Inputinteger {
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        //  Take input
+        //  Add 10 and Print
+        Scanner sc = new Scanner(System.in);
+         int a =sc.nextInt();
+         int result = a+10;
+         System.out.println(result);
+    }
+}

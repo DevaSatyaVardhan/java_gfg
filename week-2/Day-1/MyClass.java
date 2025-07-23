b410e1df0b1cdec41af921bc4c9ec3f03076cb56
Deva Satya Vardhan Chinta <devachinta051@gmail.com>
2025-07-23 19:45:15 +0530

    Create MyClass.java

---
+++ week-2/Day-1/MyClass.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+// Design a class as described in the question
+// Code here
+ class MyClass{
+    static void display(){
+        System.out.println("Hello World");
+    }
+    public static void main(String argd[]){
+        display();
+    }
+}
